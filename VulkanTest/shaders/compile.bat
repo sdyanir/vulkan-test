7e05a6e45b9b660f0bd23df17e710c73e0f976d2
sdyanir <36304605+sdyanir@users.noreply.github.com>
2018-02-10 02:41:15 +0200

    Update compile.bat
    added mkdir commands.

--- VulkanTest/shaders/compile.bat
+++ VulkanTest/shaders/compile.bat
@@ -1,4 +1,9 @@
 @echo off
+
+echo creating folders 32\ and 64\
+mkdir 32
+mkdir 64
+
 echo Compiling with 32 bit compiler to folder 32\
 echo compiling vertex shader.
 C:\VulkanSDK\1.0.65.1\Bin32\glslangValidator.exe -V -o 32\vert.spv shader.vert
